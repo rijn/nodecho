d2fdff4fedee275d92a19f725179c48ed42145e1
Rijn <rijn@Rijns-MacBook-Pro.local>
2015-05-26 14:59:42 +0800

    Adjest stylesheet

--- app.js
+++ app.js
@@ -1,10 +1,11 @@
+var globals = require('./globals');
 var express = require('express');
 var path = require('path');
 var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
-var markdown = require( "markdown" ).markdown;
+var markdown = require("markdown").markdown;
 var mongoose = require('mongoose');
 mongoose.connect('mongodb://@localhost/nodecho');
 //mongoose.connect('mongodb://nodecho_user:nodecho@localhost/nodecho');
@@ -24,7 +25,9 @@ app.set('view engine', 'ejs');
 //app.use(favicon(__dirname + '/public/favicon.ico'));
 app.use(logger('dev'));
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({
+    extended: false
+}));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
@@ -35,9 +38,9 @@ app.use(admin);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
-  var err = new Error('Not Found');
-  err.status = 404;
-  next(err);
+    var err = new Error('Not Found');
+    err.status = 404;
+    next(err);
 });
 
 // error handlers
@@ -45,23 +48,30 @@ app.use(function(req, res, next) {
 // development error handler
 // will print stacktrace
 if (app.get('env') === 'development') {
-  app.use(function(err, req, res, next) {
-    res.status(err.status || 500);
-    res.render('error', {
-      message: err.message,
-      error: err
+    app.use(function(err, req, res, next) {
+        res.status(err.status || 500);
+        res.render('error', {
+            message: err.message,
+            error: err,
+            globals: globals,
+            router: [{
+              title: "ERROR",
+              url: "",
+            }],
+        });
     });
-  });
 }
 
 // production error handler
 // no stacktraces leaked to user
 app.use(function(err, req, res, next) {
-  res.status(err.status || 500);
-  res.render('error', {
-    message: err.message,
-    error: {}
-  });
+    res.status(err.status || 500);
+    res.render('error', {
+        message: err.message,
+        error: {},
+        globals: globals,
+        router: [],
+    });
 });
 
 
