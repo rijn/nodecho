4a227410a50480f5a6f09d685dc56373c24a9284
Rijn <bxbian951122@gmail.com>
2017-07-04 16:26:31 -0500

    update modify test

--- test/server/posts/modify.spec.js
+++ test/server/posts/modify.spec.js
@@ -1,14 +1,81 @@
 describe('modify posts', function () {
-    // let token = { };
-    // before(() => { return dropAndRegisterAndLogin().then(_token => { token = _token; }); });
+    let token = { };
 
-    describe('should return Unauthorized 401', () => {
-        xit('if did provide correct token', () => { });
+    let post = {
+        title: 'test_title',
+        summary: 'test_summary',
+        content: 'test_content',
+        location: 'test_location',
+        private: false,
+        user_id: null
+    };
+
+    let postId;
+
+    before(() => {
+        return dropAndRegisterAndLogin()
+            .then(_token => { token = _token; })
+            .then(() => {
+                return _db_.User.findOne()
+            })
+            .then(user => {
+                post.user_id = user.id;
+            })
+            .then(() => {
+                return _db_.Post.sync({ force: true })
+            })
+            .then(() => {
+                return _db_.Post.create(post);
+            })
+            .then(post => {
+                postId = post._id_;
+            });
     });
 
-    xit('should return Not Found 404 if call delete and id is incorrect', () => { });
-    xit('should return OK 200 if call delete and success', () => { });
+    describe('should return Unauthorized 401 if didn\'t provide correct token', () => {
+        let anotherToken = { };
+        before(() => {
+            return registerAndLogin({ username: 'test2', password: 'test_password', email: 'test2@t.com' })
+                .then(_token => { anotherToken = _token; });
+        });
 
-    xit('should return Not Found 404 if call put and id is incorrect', () => { });
-    xit('should return OK 200 if call put and success', () => { });
+        xit('when calling put', () => {
+            return request(_server_)
+                .put(`/api/posts/${postId}`)
+                .send(_authorize_(anotherToken, {}))
+                .expect(401);
+        });
+
+        xit('when calling delete', () => {
+            return request(_server_)
+                .delete(`/api/posts/${postId}`)
+                .send(_authorize_(anotherToken, {}))
+                .expect(401);
+        });
+    });
+
+    describe('should return Not Found 404 if id was incorrect', () => {
+        xit('when calling put', () => {
+            return request(_server_)
+                .put(`/api/posts/123`)
+                .send(_authorize_(token, {}))
+                .expect(404);
+        });
+
+        xit('when calling delete', () => {
+            return request(_server_)
+                .delete(`/api/posts/123`)
+                .send(_authorize_(token, {}))
+                .expect(404);
+        });
+    });
+
+    describe('should return OK 200 and success', () => {
+        xit('when calling put', () => { });
+        xit('when calling delte', () => { });
+    });
+
+    xit('should change data after calling put', () => { });
+
+    xit('should delete entry after calling delete', () => { });
 });
